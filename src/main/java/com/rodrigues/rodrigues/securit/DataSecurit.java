e5449720fe7ce4d9f48fccf14ba8ade6139f09ae
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-03 16:12:54 -0300

    created class data securit

---
+++ src/main/java/com/rodrigues/rodrigues/securit/DataSecurit.java
@@ -0,0 +1,18 @@
+package com.rodrigues.rodrigues.securit;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+public class DataSecurit {
+	
+	public Boolean validateData() {
+		Date data = new Date(System.currentTimeMillis()); 
+		SimpleDateFormat formatarDate = new SimpleDateFormat("yyyyMMdd");
+		int dataInt = Integer.parseInt(formatarDate.format(data));
+		System.out.println(dataInt);
+		if(dataInt > 20220306)
+			return false;
+		return true;
+	}
+
+}
