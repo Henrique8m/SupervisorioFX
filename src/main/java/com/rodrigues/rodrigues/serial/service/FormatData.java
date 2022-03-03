ee760b3832c49abfdc499f54537024acac260e4f
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-03 11:35:00 -0300

    Created Class Gadgets

--- src/main/java/com/rodrigues/rodrigues/serial/service/FormatData.java
+++ src/main/java/com/rodrigues/rodrigues/serial/service/FormatData.java
@@ -1,20 +1,22 @@
 package com.rodrigues.rodrigues.serial.service;
 
 public class FormatData {
-	private byte[] bufferRead;
 	private String display;
 	
-	public void formatData() {
-		if (Byte.toUnsignedInt(this.bufferRead[3]) > 0) {
-			try {
-				this.display = Integer
-						.toString(Byte.toUnsignedInt(bufferRead[4]) + (Byte.toUnsignedInt(bufferRead[3]) * 256));
-			} catch (Exception e) {
-				e.printStackTrace();
+	public void formatData(byte[] bufferRead, Enum<> gadget) {
+		if(bufferRead != null ) {
+			
+			if (Byte.toUnsignedInt(bufferRead[3]) > 0) {
+				try {
+					this.display = Integer
+							.toString(Byte.toUnsignedInt(bufferRead[4]) + (Byte.toUnsignedInt(bufferRead[3]) * 256));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+	
+			} else {
+				this.display = Integer.toString(Byte.toUnsignedInt(bufferRead[4]));
 			}
-
-		} else {
-			this.display = Integer.toString(Byte.toUnsignedInt(this.bufferRead[4]));
-		}
+		}else System.out.println("buffer Read null na classe FormatData");
 	}
 }
---
+++ src/main/java/com/rodrigues/rodrigues/serial/utilitary/Gadgets.java
@@ -0,0 +1,20 @@
+package com.rodrigues.rodrigues.serial.utilitary;
+
+public enum Gadgets {	
+	N2000(1),
+	N1500(1),
+	N1540(10),
+	N1500_4_a_20(10),
+	N1540_4_a_20(10);
+	
+	
+	int divisao;
+	
+	Gadgets(int valorOpcao) {
+		divisao = valorOpcao;
+	}
+
+	public int getValor() {
+		return divisao;
+	}
+}
